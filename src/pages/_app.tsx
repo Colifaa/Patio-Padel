6e586c9aa3e03186261ad1e5c8b8d7110147da6b
unknown <Jorgemathez14@gmail.com>
2024-10-29 17:41:47 -0300

    NavBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
+import { ChakraProvider } from '@chakra-ui/react';
+
 import type { AppProps } from "next/app";
+import WithSubnavigation from "./Components/WithSubnavigation";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return    <ChakraProvider >
+    <WithSubnavigation/>
+  <Component {...pageProps} />
+</ChakraProvider>
 }
