e20b6701e5621eeffec3b8e92c11ace9072c0d26
unknown <Jorgemathez14@gmail.com>
2024-11-02 10:31:18 -0300

    Probbando Componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,18 @@ import { WobbleCardDemo } from "@/Components/WobbleCardDemo";
 import Inicio from "../Components/Inicio";
 
 import List from "../Components/List";
+import  {BackgroundBeamsWithCollisionDemo} from "../Components/BackgroundBeamsWithCollisionDemo";
+import { LayoutGridDemo } from "@/Components/LayoutGridDemo";
+import { AppleCardsCarouselDemo } from "@/Components/AppleCardsCarouselDemo";
 // import SplitWithImage from "../Components/SplitWithImage";
 
 export default function Home() {
   return (
     <div>
+      <AppleCardsCarouselDemo/>
+      <BackgroundBeamsWithCollisionDemo/>
       <Inicio />
+      <LayoutGridDemo/>
       <WobbleCardDemo />
       <List />
     </div>
