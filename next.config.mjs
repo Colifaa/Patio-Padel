75a55bcffb6b27b3f846f25c2377935494f59025
unknown <Jorgemathez14@gmail.com>
2024-11-02 10:34:22 -0300

    Prueba2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'assets.aceternity.com'], // Añade el nuevo dominio aquí
-  },
 };
 
 export default nextConfig;
