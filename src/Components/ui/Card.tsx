34c44250e79c1956cac97cc4005f3a6030c9c68c
unknown <Jorgemathez14@gmail.com>
2024-11-18 21:11:31 -0300

    let

--- src/Components/ui/Card.tsx
+++ src/Components/ui/Card.tsx
@@ -14,7 +14,7 @@ export const HoverEffect = ({
   }[];
   className?: string;
 }) => {
-  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null); // Cambié 'let' a 'const'
 
   return (
     <div
