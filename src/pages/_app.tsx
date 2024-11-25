2bfb150dd06dec9ebd3f034036ab85470feaef8d
unknown <Jorgemathez14@gmail.com>
2024-11-25 16:39:28 -0300

    importacion wsp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import WithSubnavigation from "../Components/WithSubnavigation";
 import ParticlesBg from "../Components/BgParticles/ParticlesBg";
-import Wsp from "@/Components/WsP";
+import Wsp from "../Components/Wsp";
 
 
 export default function App({ Component, pageProps }: AppProps) {
