09e2519e103759ba0237ec81945af750f9834aee
unknown <Jorgemathez14@gmail.com>
2024-11-27 17:16:00 -0300

    muchos cambios

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,7 +8,6 @@ function contact() {
   return (
     <div>
     <ContactSection />
-      <WobbleCardDemo />
       <Footer />
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,18 @@ import AboutCompany from "../Components/AboutCompany";
 import FAQSection from "@/Components/FAQSection";
 import Footer from "@/Components/Footer";
 import { InfiniteMovingCardsDemo } from "../Components/InfiniteMovingCardsDemo";
+import AboutUs from "@/Components/AboutUs";
 
 export default function Home() {
   return (
     <div style={{ position: "relative", zIndex: 0 }}>
       <BackgroundBeamsWithCollisionDemo />
-      <CardHoverEffectDemo />
+      <AboutUs/>
       <AboutCompany />
+      <CardHoverEffectDemo />
       <InfiniteMovingCardsDemo/>
       <FAQSection/>
-      
       <Footer />
-
     </div>
 
   );
