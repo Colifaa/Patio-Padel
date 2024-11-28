b2b5a48da5ee2b34e27e7ab4b2ce0fef54267620
unknown <Jorgemathez14@gmail.com>
2024-11-28 12:27:59 -0300

    sin button

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, SimpleGrid, chakra } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, chakra } from '@chakra-ui/react'
 import React from 'react'
 
 function Feature() {
